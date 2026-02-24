05c1e3aa62bbdab31e58711b09c355447302bead
warjiang <1096409085@qq.com>
2026-02-24 22:14:48 +0800

    docs: Add Lumina introduction PDF and prepare Vite configuration for remote development proxy.

--- playground/frontend/vite.config.ts
+++ playground/frontend/vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig({
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       },
+      // for remote development
+      // '/api': {
+      //   target: 'http://127.0.0.1:28000',
+      //   changeOrigin: true,
+      // },
     },
   },
 })
